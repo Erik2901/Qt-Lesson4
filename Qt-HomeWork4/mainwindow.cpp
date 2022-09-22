81be68a3f6e6302300ea93f55105c12200593d96
Erik2901 <erik2901@mail.ru>
2022-09-22 17:51:51 +0400

    hw4

---
+++ Qt-HomeWork4/mainwindow.cpp
@@ -0,0 +1,136 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->save_button->setText(tr("Save"));
+    ui->open_button->setText(tr("Open"));
+    ui->ref_button->setText(tr("Referance"));
+    ui->only_reading_button->setText(tr("Open Only Reading"));
+    ui->ru_button->setText(tr("Ru"));
+    ui->eng_button->setText(tr("Eng"));
+    translator = new QTranslator();
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+    delete translator;
+}
+
+
+void MainWindow::on_open_button_clicked()
+{
+    QString s = QFileDialog::getOpenFileName(this, "window", QDir::current().path(), "Text(*.txt)");
+    int index = s.indexOf(".txt");
+
+    if (s.length()!=0){
+        if(index != -1 && s.length()-4 == index){
+            QFile file(s);
+            if(file.open(QFile::ReadOnly | QFile::ExistingOnly))
+            {
+                QTextStream stream(&file);
+                ui->plainTextEdit->setPlainText(stream.readAll());
+                ui->plainTextEdit->setReadOnly(false);
+                file.close();
+            }
+        }
+    }
+}
+
+
+void MainWindow::on_save_button_clicked()
+{
+    QString s = QFileDialog::getSaveFileName(this, "window", QDir::current().path(), "Text(*.txt)");
+    if(s.length() > 0){
+        QString ext = QString(&(s.data()[s.length() - 4]));
+        if(ext==".txt"){
+            QFile file(s);
+            if(file.open(QFile::WriteOnly | QFile::NewOnly))
+            {
+                QTextStream stream(&file);
+                stream << ui->plainTextEdit->toPlainText();
+                file.close();
+            }
+        }
+    }
+}
+
+
+void MainWindow::on_ref_button_clicked()
+{
+    QFile file (":/res/referance.txt");
+    if(file.open(QFile::ReadOnly | QFile::ExistingOnly))
+    {
+        QTextStream stream(&file);
+        ui->ref_label->setText(stream.readAll());
+        file.close();
+    }
+
+}
+
+void MainWindow::on_only_reading_button_clicked()
+{
+    QString s = QFileDialog::getOpenFileName(this, "window", QDir::current().path(), "Text(*.txt)");
+    int index = s.indexOf(".txt");
+
+    if (s.length()!=0){
+        if(index != -1 && s.length()-4 == index){
+            QFile file(s);
+            if(file.open(QFile::ReadOnly | QFile::ExistingOnly))
+            {
+                QTextStream stream(&file);
+                ui->plainTextEdit->setPlainText(stream.readAll());
+                ui->plainTextEdit->setReadOnly(true);
+                file.close();
+            }
+        }
+    }
+}
+
+
+void MainWindow::on_ru_button_clicked()
+{
+    lang_switch("ru");
+}
+
+
+void MainWindow::on_eng_button_clicked()
+{
+    lang_switch("en");
+}
+
+void MainWindow::lang_switch(QString lang){
+    translator->load(":/res/switchlang_" + lang + ".qm");
+    qApp->installTranslator(translator);
+    ui->save_button->setText(tr("Save"));
+    ui->open_button->setText(tr("Open"));
+    ui->ref_button->setText(tr("Referance"));
+    ui->only_reading_button->setText(tr("Open Only Reading"));
+    ui->ru_button->setText(tr("Ru"));
+    ui->eng_button->setText(tr("Eng"));
+}
+
+void MainWindow::keyReleaseEvent(QKeyEvent *event)
+{
+
+    if(event->key() == Qt::Key_O && event->modifiers() == Qt::ControlModifier){
+        on_open_button_clicked();
+    }
+    if(event->key() == Qt::Key_S && event->modifiers() == Qt::ControlModifier){
+        on_save_button_clicked();
+    }
+    if(event->key() == Qt::Key_N && event->modifiers() == Qt::ControlModifier){
+        ui->plainTextEdit->clear();
+        ui->plainTextEdit->setReadOnly(false);
+    }
+    if(event->key() == Qt::Key_Q && event->modifiers() == Qt::ControlModifier){
+        qApp->quit();
+    }
+}
+
+
---
+++ Qt-HomeWork4/switchlang_ru.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="37"/>
+        <source>Exercise 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="50"/>
+        <location filename="mainwindow.cpp" line="9"/>
+        <location filename="mainwindow.cpp" line="111"/>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="63"/>
+        <location filename="mainwindow.cpp" line="10"/>
+        <location filename="mainwindow.cpp" line="112"/>
+        <source>Open</source>
+        <translation>Открыть</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="76"/>
+        <location filename="mainwindow.cpp" line="11"/>
+        <location filename="mainwindow.cpp" line="113"/>
+        <source>Referance</source>
+        <translation>Справка</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="102"/>
+        <location filename="mainwindow.cpp" line="12"/>
+        <location filename="mainwindow.cpp" line="114"/>
+        <source>Open Only Reading</source>
+        <translation>Открыть только для чтения</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="115"/>
+        <source>ru</source>
+        <translation>Рус</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="128"/>
+        <source>eng</source>
+        <translation>Англ</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="13"/>
+        <location filename="mainwindow.cpp" line="115"/>
+        <source>Ru</source>
+        <translation>Рус</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="14"/>
+        <location filename="mainwindow.cpp" line="116"/>
+        <source>Eng</source>
+        <translation>Англ</translation>
+    </message>
+</context>
+</TS>
